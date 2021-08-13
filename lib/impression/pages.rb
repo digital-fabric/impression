9d24f26346944f52777c8a41e53e75219217c92e
Sharon Rosner <sharon@noteflakes.com>
2021-08-13 11:52:48 +0000

    Improve Pages
    - Add layouts
    - Add support for Rubyoshka pages
    - Add automatic reload (WIP)

--- lib/impression/pages.rb
+++ lib/impression/pages.rb
@@ -4,6 +4,8 @@ require 'kramdown'
 require 'rouge'
 require 'kramdown-parser-gfm'
 require 'yaml'
+require 'rb-inotify'
+require 'rubyoshka'
 
 require_relative './errors'
 
@@ -16,10 +18,56 @@ module Impression
     end
 
     def initialize(base_path, opts = {})
+    p base_path: base_path
       @base_path = base_path
       @opts = opts
+      @opts[:pages] = self
 
       load
+
+      if opts[:auto_reload]
+        start_automatic_reloader
+      end
+    end
+
+    def start_automatic_reloader
+      notifier = INotify::Notifier.new
+      watched = {}
+      @map.each_value do |entry|
+        path = entry[:full_path]
+        next if watched[path]
+
+        notifier.watch(path, :modify, :delete_self) { |e| handle_changed_file(path) }
+        watched[path] = true
+      end
+      notifier.watch(@base_path, :moved_to, :create) do |event|
+        path = event.absolute_name
+        if File.file?(path)
+          notifier.watch(path, :modify, :delete_self) { |e| handle_changed_file(path) }
+        end
+        handle_changed_file(path)
+      end
+      @reloader = spin do
+        notify_io = notifier.to_io
+        loop do
+          notify_io.wait_readable
+          notifier.process
+        end
+      end
+    end
+
+    def handle_changed_file(full_path)
+      p handle_changed_file: full_path
+      if !File.file?(full_path)
+        @map.reject! { |k, v| v[:full_path] == full_path }
+        return
+      end
+
+      path = File.basename(full_path)
+      page = Page.new(path, full_path, @opts)
+      permalink = page.permalink
+      @map[permalink] = { page: page, full_path: full_path }
+      @map['/'] = @map[permalink] if permalink == '/index'
     end
 
     def load
@@ -31,12 +79,9 @@ module Impression
         next unless File.file?(full_path)
 
         page = Page.new(path, full_path, @opts)
-        @map[page.relative_permalink] = page
+        @map[page.permalink] = { page: page, full_path: full_path }
       end
       @map['/'] = @map['/index']
-
-      puts '* loaded pages' * 40
-      p page_keys: @map.keys
     end
     alias_method :reload, :load
 
@@ -45,91 +90,118 @@ module Impression
     end
 
     def serve(req)
-      p serve: req.route_relative_path
-      page = @map[req.route_relative_path]
-      raise NotFoundError unless page
+      entry = @map[req.route_relative_path]
+      raise NotFoundError unless entry
 
-      req.respond(page.to_html, 'Content-Type' => 'text/html')
+      body = render_page(entry[:page])
+      req.respond(body, 'Content-Type' => 'text/html')
+    rescue NotFoundError => e
+      req.respond('Not found.', ':status' => e.http_status)
+    end
+    alias_method :call, :serve
+
+    def render_page(page)
+      layout_proc(page.layout).().render(pages: self, page: page)
     end
 
-    class Page
-      def initialize(path, full_path, opts = {})
-        @path = path
-        @full_path = full_path
-        @opts = opts
-        read_page
-      end
-    
-      PAGE_REGEXP = /\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)/m.freeze
-    
-      def read_page
-        data = IO.read(@full_path) || ''
-        if data =~ PAGE_REGEXP
-          front_matter = Regexp.last_match(1)
-          @attributes = YAML.load(front_matter)
-          @content = Regexp.last_match.post_match
-        else
-          @attributes = {}
-          @content = data
-        end
-      end
+    def layout_proc(layout)
+      full_path = File.expand_path("../_layouts/#{layout}.rb", @base_path)
+      instance_eval("->(&block) do; #{IO.read(full_path)}; end", full_path)
+    end
 
-      def relative_permalink
-        @relative_permalink = @attributes[:permalink] || path_without_extension
+    def select(selector)
+      @map.inject([]) do |array, (permalink, entry)|
+        array << entry[:page] if permalink =~ selector
+        array
       end
+    end
+  end
 
-      def path_without_extension
-        "/#{File.basename(@path, File.extname(@path))}"
-      end
-    
-      def title
-        @attributes['title'] || title_from_content
-      end
-    
-      TITLE_REGEXP = /^#\s+([^\n]+)/.freeze
-    
-      def title_from_content
-        (@content =~ TITLE_REGEXP) && Regexp.last_match(1)
-      end
-    
-      def status
-        @attributes['status'] || Qeweney::Status::OK
-      end
-    
-      def to_html
-        @html ||= render_html
-      end
+  class Page
+    attr_reader :attributes
 
-      def kramdown_options
-        {
-          entity_output: :numeric,
-          syntax_highlighter: :rouge,
-          input: 'GFM'
-        }
-      end
+    def initialize(path, full_path, opts = {})
+      @path = path
+      @full_path = full_path
+      @opts = opts
+      @kind = detect_page_kind(full_path)
+      read_page
+    end
 
-      def render_html
-        html_layout { Kramdown::Document.new(@content, **kramdown_options).to_html }
-      end
+    EXTNAME_REGEXP = /^\.(.+)$/.freeze
 
-      def html_layout
-        <<~HTML
-        <!DOCTYPE html>
-        <html lang="en-us">
-          <head>
-            <title>#{title}</title>
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="chrome=1">
-            <meta name="HandheldFriendly" content="True">
-            <meta name="MobileOptimized" content="320">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta name="referrer" content="no-referrer">
-            <link rel="stylesheet" href="/assets/style.css">
-          </head>
-          <body>#{yield}</body>
-        </html>
-        HTML
+    def detect_page_kind(path)
+      File.extname(path).match(EXTNAME_REGEXP)[1].to_sym
+    end
+  
+    PAGE_REGEXP = /\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)/m.freeze
+  
+    def read_page
+      data = IO.read(@full_path) || ''
+      if data =~ PAGE_REGEXP
+        front_matter = Regexp.last_match(1)
+        @content_start_line = front_matter.lines.size + 2
+        @attributes = YAML.load(front_matter)
+        @content = Regexp.last_match.post_match
+      else
+        @attributes = {}
+        @content_start_line = 1
+        @content = data
       end
+    end
+
+    def permalink
+      @permalink = @attributes[:permalink] || path_without_extension
+    end
+
+    def path_without_extension
+      "/#{@path.delete_suffix(File.extname(@path))}"
+    end
+  
+    def title
+      @attributes['title'] || title_from_content
+    end
+  
+    TITLE_REGEXP = /^#\s+([^\n]+)/.freeze
+  
+    def title_from_content
+      (@content =~ TITLE_REGEXP) && Regexp.last_match(1)
+    end
+  
+    def status
+      @attributes['status'] || Qeweney::Status::OK
+    end
+
+    def layout
+      layout = @attributes['layout'] || 'default'
+    end
+  
+    def render
+      case @kind
+      when :md
+        render_markdown
+      when :rb
+        render_rubyoshka
+      else
+        raise "Invalid page kind #{kind.inspect}"
+      end
+    end
+
+    def render_markdown
+      Kramdown::Document.new(@content, **kramdown_options).to_html
+    end
+
+    def kramdown_options
+      {
+        entity_output: :numeric,
+        syntax_highlighter: :rouge,
+        input: 'GFM'
+      }
+    end
+
+    def render_rubyoshka
+      proc = instance_eval("->(&block) do; #{@content}; end", @full_path, @content_start_line)
+      proc.().render(page: self, pages: @opts[:pages])
     end
   end
 end
