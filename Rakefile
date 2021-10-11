1f4cb70f8c3c7658f3fa7db2b0ab0e767760a40f
Sharon Rosner <sharon@noteflakes.com>
2021-10-11 08:41:15 +0000

    Setup testing

---
+++ Rakefile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "bundler/gem_tasks"
+require "rake/clean"
+
+task :default => [:test]
+
+task :test do
+  exec 'ruby test/run.rb'
+end
---
+++ test/helper.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require_relative './coverage' if ENV['COVERAGE']
+require 'minitest/autorun'
+
+module Kernel
+  def capture_exception
+    yield
+  rescue Exception => e
+    e
+  end
+
+  def trace(*args)
+    STDOUT.orig_write(format_trace(args))
+  end
+
+  def format_trace(args)
+    if args.first.is_a?(String)
+      if args.size > 1
+        format("%s: %p\n", args.shift, args)
+      else
+        format("%s\n", args.first)
+      end
+    else
+      format("%p\n", args.size == 1 ? args.first : args)
+    end
+  end
+end
+
+module Minitest::Assertions
+  def assert_in_range exp_range, act
+    msg = message(msg) { "Expected #{mu_pp(act)} to be in range #{mu_pp(exp_range)}" }
+    assert exp_range.include?(act), msg
+  end
+end
