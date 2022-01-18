64b9d4d59e3788db3be84142cce80c26e092fe4e
Sharon Rosner <sharon@noteflakes.com>
2022-01-18 21:39:48 +0100

    Further refactor FileTree path_info calculation

--- lib/impression/file_tree.rb
+++ lib/impression/file_tree.rb
@@ -24,7 +24,7 @@ module Impression
     # @return [void]
     def call(req)
       path_info = get_path_info(req.resource_relative_path)
-      render_from_path_info(req, *path_info)
+      render_from_path_info(req, path_info)
     end
 
     private
@@ -34,12 +34,12 @@ module Impression
     # @param req [Qeweney::Request] request
     # @param kind [Symbol] path kind (`:not_found` or `:file`)
     # @param path [String, nil] file path
-    def render_from_path_info(req, kind, path = nil)
-      case kind
+    def render_from_path_info(req, path_info)
+      case path_info[:kind]
       when :not_found
         req.respond(nil, ':status' => Qeweney::Status::NOT_FOUND)
       when :file
-        req.serve_file(path)
+        req.serve_file(path_info[:path])
       else
         raise "Invalid path info kind #{kind.inspect}"
       end
@@ -58,10 +58,10 @@ module Impression
     # @param path [String] relative path
     # @param add_ext [bool] whether to add .html extension if not found
     # @return [Array] path info
-    def calculate_path_info(path, add_ext = true)
+    def calculate_path_info(path)
       full_path = File.join(@directory, path)
 
-      path_info(full_path) || (add_ext && path_info("#{full_path}.html")) || [:not_found]
+      path_info(full_path) || search_path_info_with_extension(full_path) || { kind: :not_found }
     end
 
     private
@@ -73,7 +73,7 @@ module Impression
       elsif stat.directory?
         return directory_path_info(path)
       else
-        return [:file, path]
+        return { kind: :file, path: path, ext: File.extname(path) }
       end
     end
 
@@ -83,7 +83,19 @@ module Impression
     # @param path [String] directory path
     # @return [Array] path info
     def directory_path_info(path)
-      path_info(File.join(path, 'index.html'))
+      search_path_info_with_extension(File.join(path, 'index'))
+    end
+
+    def supported_path_extensions
+      [:html]
+    end
+
+    def search_path_info_with_extension(path)
+      supported_path_extensions.each do |ext|
+        info = path_info("#{path}.#{ext}")
+        return info if info
+      end
+      nil
     end
   end
 end
